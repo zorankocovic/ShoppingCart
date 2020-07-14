41c9593a1cac997597e0c1e99b31832215e3c2aa
Zoran Kocovic <59580876+zorankocovic@users.noreply.github.com>
2020-07-14 20:22:34 +0200

    Add files via upload

---
+++ app/src/main/java/mobile/shoppingcart/adapter/StoreAdapter.kt
@@ -0,0 +1,85 @@
+package mobile.shoppingcart.adapter
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
+import androidx.recyclerview.widget.RecyclerView;
+import android.util.Log
+import android.widget.*
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+import mobile.shoppingcart.AddStore
+import mobile.shoppingcart.R
+import mobile.shoppingcart.model.Store
+import java.util.ArrayList
+import android.view.ContextMenu.ContextMenuInfo
+import android.view.*
+
+class StoreAdapter (productList: List<Store>, internal var context: Context) : RecyclerView.Adapter<StoreAdapter.ViewHolder>() {
+
+    internal var productList: List<Store> = ArrayList()
+    init {
+        this.productList = productList
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.list_store, parent, false)
+        return ViewHolder(view)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN)
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val storeproduct = productList[position]
+        holder.name.text = storeproduct.name
+
+        holder.itemView.setOnClickListener {
+            val i = Intent(context, AddStore::class.java)
+            i.putExtra("Mode", "E")
+            i.putExtra("Id", storeproduct.id)
+
+            i.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            //  context.startActivity(i)
+        }
+        holder.buttonViewOption?.setOnClickListener(View.OnClickListener {
+
+
+            val pop= PopupMenu(context,it)
+            pop.inflate(R.menu.optionsshoppingcart)
+
+            pop.setOnMenuItemClickListener {item->
+
+                when(item.itemId)
+
+                {
+                    R.id.menu1->{Toast.makeText(context,storeproduct.id.toString(), Toast.LENGTH_SHORT).show() }
+
+                    R.id.menu3->{ }
+
+
+                }
+                true
+            }
+            pop.show()
+            true
+
+
+        })
+
+
+
+    }
+
+    override fun getItemCount(): Int {
+        return productList.size
+    }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var name: TextView = view.findViewById(R.id.name) as TextView
+        var buttonViewOption: Button = view.findViewById(R.id.textViewOptions) as Button
+
+        var list_item: RelativeLayout = view.findViewById(R.id.list_item) as RelativeLayout
+
+    }
+
+
+}
