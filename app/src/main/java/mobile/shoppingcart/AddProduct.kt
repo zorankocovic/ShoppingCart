5e3b5096a9093b5e2b1a78a97d4db7c61dd9d5a8
Zoran Kocovic <59580876+zorankocovic@users.noreply.github.com>
2021-07-17 21:14:47 +0200

    Update AddProduct.kt

--- app/src/main/java/mobile/shoppingcart/AddProduct.kt
+++ app/src/main/java/mobile/shoppingcart/AddProduct.kt
@@ -1,180 +1,80 @@
-package mobile.shoppingcart
+package mobile.kotlinlogin
+
+import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AlertDialog
-import androidx.appcompat.app.AppCompatActivity
-import android.util.Log
-import android.view.MenuItem
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
-import mobile.shoppingcart.db.DatabaseHandler
-import mobile.shoppingcart.model.StoreProduct
-import kotlinx.android.synthetic.main.activity_add_edit.*
-import android.database.sqlite.SQLiteDatabase
-import android.database.sqlite.SQLiteOpenHelper
-import mobile.shoppingcart.model.Store
+import androidx.appcompat.app.AppCompatActivity
+import mobile.kotlinlogin.model.LoginModel
+import mobile.kotlinlogin.rest.ApiLogin
+import kotlinx.android.synthetic.main.login.*
+import kotlinx.coroutines.*
+import mobile.kotlinlogin.rest.AppPreferences
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.util.*
 
-class AddProduct : AppCompatActivity(), AdapterView.OnItemSelectedListener {
-    var strs= IntArray(1)
-    var storespinner: ArrayList<String> = ArrayList()
-     val NEW_SPINNER_ID = 1
-    var dbHandler: DatabaseHandler? = null
-    var isEditMode = false
-    var selectedstore:String=""
-    public override fun onCreate(savedInstanceState: Bundle?) {
+class LoginActivity : AppCompatActivity() {
+    var dataList = ArrayList<LoginModel>()
+    var uemail:String=""
+    var upass:String=""
+    var successlogin:String = "0"
+    private var typeText: TextView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_edit)
+        setContentView(R.layout.login)
+        AppPreferences.init(this)
+       if(AppPreferences.isLogin){
+           val intent = Intent(this, MainActivity::class.java)
+           // start your next activity
+           startActivity(intent)
 
-        //if (intent == null)populatespinner()
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-      /*  val mySpinner = findViewById<Spinner>(R.id.istorename)
-        var aa = ArrayAdapter(this, android.R.layout.simple_spinner_item, languages)
-        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+           finish()
+       }
 
-        with(mySpinner)
-        {
-            adapter = aa
-            setSelection(0, false)
-            onItemSelectedListener = this@AddProduct
-            prompt = "Select your favourite language"
-            gravity = android.view.Gravity.CENTER
+        val btn_login = findViewById(R.id.button2) as Button
+// set on-click listener
+        btn_login.setOnClickListener {
 
-        }*/
+            uemail=useremail.text.toString()
+            upass=  userpass.text.toString()
 
-        initDB()
-        initOperations()
+            getDat1a()
 
-    }
-
-    private fun initDB() {
-        dbHandler = DatabaseHandler(this)
-        btn_delete.visibility = View.INVISIBLE
-        if (intent != null && intent.getStringExtra("Mode") == "E") {
-            isEditMode = true
-            val storeproduct: StoreProduct = dbHandler!!.getProduct(intent.getIntExtra("Id",0))
-          //  istorename.setText(storeproduct.store)
-            populatespinner(storeproduct.store)
-            iname.setText(storeproduct.product)
-            idesc.setText(storeproduct.price)
-
-            btn_delete.visibility = View.VISIBLE
         }
-        else {populatespinner("")}
     }
+    private fun getDat1a() {
 
-    private fun initOperations() {
-        btn_save.setOnClickListener({
-            var success: Boolean = false
-            if (!isEditMode) {
-                val storeproduct: StoreProduct = StoreProduct()
-                storeproduct.store = selectedstore
-                storeproduct.product = iname.text.toString()
-                storeproduct.price = idesc.text.toString()
+        val call: Call<List<LoginModel>> = ApiLogin.getClient.userlogin(uemail,upass)
+        llProgressBar.visibility = View.VISIBLE
+        call.enqueue(object : Callback<List<LoginModel>> {
 
-                success = dbHandler?.addProduct(storeproduct) as Boolean
-            } else {
+            override fun onResponse(call: Call<List<LoginModel>>?, response: Response<List<LoginModel>>?) {
+               successlogin= response?.body()?.get(0)?.success.toString()
 
-                val tasks: StoreProduct = StoreProduct()
-                tasks.id = intent.getIntExtra("Id", 0)
-               tasks.store = selectedstore
-                tasks.product = iname.text.toString()
-                tasks.price = idesc.text.toString()
+                if(successlogin.equals("1")) {
 
-                success = dbHandler?.updateProduct(tasks) as Boolean
+                    AppPreferences.isLogin = true
+                    val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                    // start your next activity
+                    startActivity(intent)
+                    llProgressBar.visibility = View.GONE
+                    finish()
+
+                }
+                else
+                {  llProgressBar.visibility = View.GONE
+                    Toast.makeText(applicationContext,"Wrong login data", Toast.LENGTH_SHORT).show()}
             }
 
-            if (success)
-                finish()
-        })
+            override fun onFailure(call: Call<List<LoginModel>>?, t: Throwable?) {
+                Toast.makeText(applicationContext,"something wrong", Toast.LENGTH_SHORT).show()
 
-        btn_delete.setOnClickListener({
-            val dialog = AlertDialog.Builder(this).setTitle("Info").setMessage("Click 'YES' Delete the Task.")
-                .setPositiveButton("YES", { dialog, i ->
-                    val success = dbHandler?.deleteProduct(intent.getIntExtra("Id", 0)) as Boolean
-                    if (success)
-                        finish()
-                    dialog.dismiss()
-                })
-                .setNegativeButton("NO", { dialog, i ->
-                    dialog.dismiss()
-                })
-            dialog.show()
-        })
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val id = item.itemId
-        if (id == android.R.id.home) {
-            finish()
-            return true
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
-    fun populatespinner(ab:String)
-    {   //  var languages = arrayOf("Java", "PHP", "Kotlin", "Javascript", "Python", "Swift")
-        //var languages = arrayOf("Java", "PHP", "Kotlin", "Javascript", "Python", "Swift")
-var selectedposition: Int = 0
-        var languages: ArrayList<Store> = ArrayList()
-        //list.add("text")
-        dbHandler = DatabaseHandler(this)
-        languages = (dbHandler as DatabaseHandler).allStorespinner()
-        var num:Int = languages.size
-      //  Toast.makeText(applicationContext,num.toString(), Toast.LENGTH_SHORT).show()
-        strs= IntArray(languages.size)
-        for (i in 0 until num) {
-            val storeproduct = languages[i]
-            strs[i]= storeproduct.id
-            storespinner.add(storeproduct.name)
-if(i==0)selectedstore=storespinner[i]
-       if(storeproduct.name==ab)
-           selectedposition=i;
-        }
-
-
-       /* val db = this.writableDatabase
-        val selectQuery = "SELECT  * FROM Store"
-        val cursor = db.rawQuery(selectQuery, null)
-        if (cursor != null) {
-            if (cursor.moveToFirst()) {
-                do {
-
-                } while (cursor.moveToNext())
             }
-        }
-        cursor.close()*/
 
-
-
-
-        val mySpinner = findViewById<Spinner>(R.id.istorename)
-        var aa = ArrayAdapter(this, android.R.layout.simple_spinner_item, storespinner)
-        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
-        with(mySpinner)
-        {
-            adapter = aa
-            setSelection(selectedposition, false)
-            onItemSelectedListener = this@AddProduct
-            prompt = "Select your favourite language"
-            gravity = android.view.Gravity.CENTER
-
-        }
-
+        })
     }
-
-    override fun onNothingSelected(parent: AdapterView<*>?) {
-
-    }
-
-     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-       //  Toast.makeText(applicationContext,strs[position].toString(), Toast.LENGTH_SHORT).show()
-        // Toast.makeText(applicationContext,storespinner[position], Toast.LENGTH_SHORT).show()
-         selectedstore=storespinner[position]
-
-     }
-     }
-
-
+}
